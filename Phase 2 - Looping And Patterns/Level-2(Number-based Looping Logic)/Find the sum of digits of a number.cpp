6d5eab4e25e8d94b5d08baf6f64270909bf0f404
aryanjha15 <onlineclass9582@gmail.com>
2026-07-24 22:10:55 +0530

    ok

---
+++ Phase 2 - Looping And Patterns/Level-2(Number-based Looping Logic)/Find the sum of digits of a number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, sum=0;
+    cout << "Enter a number: ";
+    cin >> n;
+    while(n > 0){
+        int digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    cout << "The sum of digits is: " << sum << endl;
+    return 0;
+}
