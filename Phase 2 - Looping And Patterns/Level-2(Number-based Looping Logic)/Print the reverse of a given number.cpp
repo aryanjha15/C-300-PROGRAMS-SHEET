7bb101666653e8c447441bebbe53f22d6dee01ed
aryanjha15 <onlineclass9582@gmail.com>
2026-07-25 09:36:16 +0530

    ok

---
+++ Phase 2 - Looping And Patterns/Level-2(Number-based Looping Logic)/Print the reverse of a given number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, reverse=0;
+    cout << "Enter a number: ";
+    cin >> n;
+    while(n > 0){
+        int digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+    cout << "The reverse of the number is: " << reverse << endl;
+    return 0;
+}
