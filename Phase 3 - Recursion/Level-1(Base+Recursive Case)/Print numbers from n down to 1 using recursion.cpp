5038c50d2760ebf9048f46016f189b2ba51e1e49
aryanjha15 <onlineclass9582@gmail.com>
2026-07-26 21:29:39 +0530

    done

--- Phase 3 - Recursion/Level-1(Base+Recursive Case)/Print numbers from n down to 1 using recursion.cpp
+++ Phase 3 - Recursion/Level-1(Base+Recursive Case)/Print numbers from n down to 1 using recursion.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 void print(int n){
     if(n==0) return; 
-    print(n-1);
     cout<<n<<endl;
+    print(n-1);
      }
-int main(){
+     int main(){
     int n;
     cout<<"Enter n :";
     cin>>n;
     print(n);
+    return 0;
 }
-    
-    
-        
-    
