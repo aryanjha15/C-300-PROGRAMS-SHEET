20d1c6d58ac508e53d87f367cf05a81ee1426eb9
aryanjha15 <onlineclass9582@gmail.com>
2026-07-30 12:11:26 +0530

    Reverse

---
+++ Phase 3 - Recursion/Level - 2 (Number - based Recursive Thinking)/Reverse a number recursively.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void reverseNumber(int n) {
+    if (n == 0) return;
+    cout << n % 10;
+    reverseNumber(n / 10);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Reversed number: ";
+    reverseNumber(n);
+    cout << endl;
+    return 0;
+}
