03994099611f0872c90cec947804aa6b71c9070a
aryanjha15 <onlineclass9582@gmail.com>
2026-07-30 11:40:39 +0530

    done

---
+++ Phase 3 - Recursion/Level - 2 (Number - based Recursive Thinking)/Count the number of digits in a number recursively.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int countDigits(int n) {
+    if (n == 0) return 0;
+    return 1 + countDigits(n / 10);
+}
+int main() {
+    int n;
+    cout << "Enter Number: ";
+    cin >> n;
+    cout << "Count of digits: " << countDigits(n) << endl;
+    return 0;
+}
