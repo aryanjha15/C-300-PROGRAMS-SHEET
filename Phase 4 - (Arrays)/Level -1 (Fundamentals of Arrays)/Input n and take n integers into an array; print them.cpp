246b2711df70e8954a065b0f5bd77b072b2af614
aryanjha15 <onlineclass9582@gmail.com>
2026-07-31 21:11:18 +0530

    Done

---
+++ Phase 4 - (Arrays)/Level -1 (Fundamentals of Arrays)/Input n and take n integers into an array; print them.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter "<<n<<" integers: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"The elements in the array are: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
