164c71adb7a345865e636670dfb1e39df392a167
aryanjha15 <onlineclass9582@gmail.com>
2026-08-01 17:39:30 +0530

    done

---
+++ Phase 4 - Arrays/Level - 4 Aggregate and Comparative Thinking/Print all elements that appear more than once.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter "<<n<<" integers for the first array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }  
+    int brr[n];
+    cout<<"Enter "<<n<<" integers for the second array: ";
+    for(int i=0; i<n; i++){
+        cin>>brr[i];
+    }
+
+    cout<<"The elements that appear more than once in the first array are: ";
+
+    for(int i=0; i<n; i++){
+        int count = 0;
+        for(int j=0; j<n; j++){
+            if(arr[i] == arr[j]){
+                count++;
+            }
+        }
+        if(count > 1){
+            cout<<arr[i]<<" ";
+        }
+
+    }
+
+    cout<<"\nThe elements that appear more than once in the second array are: ";
+    for(int i=0; i<n; i++){
+        int count = 0;
+        for(int j=0; j<n; j++){
+            if(brr[i] == brr[j]){
+                count++;
+            }
+        }
+        if(count > 1){
+            cout<<brr[i]<<" ";
+        }
+
+    }
+
+
+    cout<<endl;
+    return 0;
+}
