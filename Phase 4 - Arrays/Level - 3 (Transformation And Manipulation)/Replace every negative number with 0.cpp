99325109f02db41832d65256c1994eeefa9a8a7e
aryanjha15 <onlineclass9582@gmail.com>
2026-08-01 10:03:11 +0530

    done

---
+++ Phase 4 - Arrays/Level - 3 (Transformation And Manipulation)/Replace every negative number with 0.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;     
+    int arr[n];
+    cout<<"Enter "<<n<<" integers: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++){
+        if(arr[i] < 0){
+            arr[i] = 0;
+        }
+    }
+    cout<<"The array after replacing negative numbers with 0 is: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
