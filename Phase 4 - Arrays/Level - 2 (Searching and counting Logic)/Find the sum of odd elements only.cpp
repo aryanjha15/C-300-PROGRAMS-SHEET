585fec84799777ab06ba413ffd71e750d95173eb
aryanjha15 <onlineclass9582@gmail.com>
2026-08-01 08:49:09 +0530

    done

---
+++ Phase 4 - Arrays/Level - 2 (Searching and counting Logic)/Find the sum of odd elements only.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter "<<n<<" integers: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        if(arr[i] % 2 != 0){
+            sum += arr[i];
+        }
+    }
+    cout<<"The sum of odd elements in the array is: "<<sum<<endl;
+    return 0;
+}
