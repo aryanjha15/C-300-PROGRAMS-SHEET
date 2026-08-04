40d91fff478ab69cecf41c546333b8cc1e3a6da2
aryanjha15 <onlineclass9582@gmail.com>
2026-08-04 21:41:07 +0530

    done

---
+++ Phase 5 - Strings/Level - 1 Basic String Handling/Take two strings and print them concatenated.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str1, str2;
+    cout<<"Enter the first string: ";
+    getline(cin, str1);
+    cout<<"Enter the second string: ";
+    getline(cin, str2);
+    cout<<"The concatenated string is: "<<str1+str2;
+    return 0;
+}
