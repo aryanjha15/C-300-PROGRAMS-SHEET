2af481187b22911f6a4f6c2f4f9e05ee70d6b868
aryanjha15 <onlineclass9582@gmail.com>
2026-08-04 21:33:01 +0530

    done

---
+++ Phase 5 - Strings/Level - 1 Basic String Handling/Convert all characters of a string to uppercase.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    for(int i=0; i<str.length(); i++){
+        str[i] = toupper(str[i]);
+    }
+    cout<<"The string in uppercase is: "<<str;
+    return 0;
+}
