30ff55c61351585f709c9d2fbaed837c9727e8b7
aryanjha15 <onlineclass9582@gmail.com>
2026-08-05 02:31:38 +0530

    FREQUENCY

---
+++ Phase 5 - Strings/Level - 2 Counting And Character Analysis/Find the frequency of each character in a string (without using a map).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    int freq[256] = {0};
+    for(int i=0; i<str.length(); i++){
+        freq[str[i]]++;
+    }
+    cout<<"Character frequencies:"<<endl;
+    for(int i=0; i<256; i++){
+        if(freq[i]>0){
+            cout<<char(i)<<": "<<freq[i]<<endl;
+        }
+    }
+    return 0;
+}
