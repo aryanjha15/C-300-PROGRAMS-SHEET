2d5c3e1da22f7bdc94f7664a3415d8965035a7f1
aryanjha15 <onlineclass9582@gmail.com>
2026-08-04 21:39:28 +0530

    done

---
+++ Phase 5 - Strings/Level - 1 Basic String Handling/Count how many words are in a sentence.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    int count = 0;
+    for(int i=0; i<str.length(); i++){
+        if(str[i] != ' '){
+            count++;
+        }
+    }
+    cout<<"The number of words in the string is: "<<count;
+    return 0;
+}
