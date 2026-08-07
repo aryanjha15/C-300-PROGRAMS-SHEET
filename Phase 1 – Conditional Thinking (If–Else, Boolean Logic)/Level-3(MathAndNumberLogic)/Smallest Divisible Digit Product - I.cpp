e71f16519e9c57700cf9784766976ee04f491d8d
aryanjha15 <onlineclass9582@gmail.com>
2026-08-07 23:12:17 +0530

    Done

---
+++ Phase 1 – Conditional Thinking (If–Else, Boolean Logic)/Level-3(MathAndNumberLogic)/Smallest Divisible Digit Product - I.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, t;
+   cout << "Enter a number: ";
+    cin >> n;
+    cout << "Enter a divisor: ";
+    cin >> t;
+
+    
+    while (true) {
+        int product = 1;
+        int temp = n;
+        while (temp > 0) {
+            product *= (temp % 10);
+            temp /= 10;
+        }
+        if (product % t == 0) {
+            cout << n << endl;
+            break;
+        }
+        n++;
+    }
+
+    cout << "The smallest number greater than or equal to the given number whose product of digits is divisible by " << t << " is: " << n << endl;
+
+    
+    
+    return 0;
+}
