1f6a175147fbbff05dadbda1b3d6ba58694cac2e
aryanjha15 <onlineclass9582@gmail.com>
2026-09-04 10:50:06 +0530

    done

---
+++ Mixed Practice/divisibleby5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number";
+    cin>>n;
+    if(n%5==0){
+        cout<<"Divisible by 5";
+    }
+    else{
+        cout<<"not divisible by 5";
+    }
+}
