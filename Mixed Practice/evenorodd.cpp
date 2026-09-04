584b7215c3df656c26c3fb3b6b56e8dae9afdd02
aryanjha15 <onlineclass9582@gmail.com>
2026-09-04 10:45:57 +0530

    done

---
+++ Mixed Practice/evenorodd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number :";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number is even";
+    }
+    else{
+        cout<<"Number is odd";
+        
+    }
+}
