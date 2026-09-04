1e4d4a1d5361fa8f64df70d94aeac369296d4eaa
aryanjha15 <onlineclass9582@gmail.com>
2026-09-04 11:14:48 +0530

    Done

---
+++ Mixed Practice/largernumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a";
+    cin>>a;
+    int b;
+    cout<<"Enter b";
+    cin>>b;
+    if(a>b){
+        cout<<"a is larger";
+    }
+    else{
+        cout<<"b is larger";
+    }
+}
