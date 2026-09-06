ac84f866c2808e001c7a4e30522faf1bc5e32366
aryanjha15 <onlineclass9582@gmail.com>
2026-09-06 08:46:15 +0530

    done

---
+++ Mixed Practice/thirdangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter angle 1 : ";
+    cin>>a;
+    cout<<"Enter angle 2 : ";
+    cin>>b;
+    cout<<"angle 3 : "<<180-a-b;
+}
