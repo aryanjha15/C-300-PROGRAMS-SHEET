e2bcc5198a1a141cec4d69f89e97d39e7d1b71dd
aryanjha15 <onlineclass9582@gmail.com>
2026-09-06 08:52:46 +0530

    done

--- Mixed Practice/evenlydivideamount.cpp
+++ Mixed Practice/evenlydivideamount.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
-#include<cmath>~
 using namespace std;
 int main(){
     int n;
     cout<<"Enter Number : ";
     cin>>n;
-    int r = sqrt(n);
-    cout << (r*r == n ? "Perfect Square" : "Not Perfect Square"); 
+    cout<<(n%100==0?"Yes":"No");
 }
